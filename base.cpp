6c8850b820220ebfe407f2ed64b95add35a72074
Alejandro <ale_arc2015@hotmail.com>
2019-03-28 18:56:59 -0300

    CPP Base

--- base.cpp
+++ base.cpp
@@ -4,29 +4,29 @@
 enum TRaza{Orco, Humano, Mago, Enano, Elfo};
 
 typedef struct TDatos {
-	TRaza Raza; //nota 1
-	Char *ApellidoNombre; //nota 2
+	TRaza raza; //nota 1
+	Char * apellidonombre; //nota 2
 	int edad; //entre 0 a 300
-	double Salud://100
+	double salud://100
 }tdatos;
 
 typedef struct TCaracteristicas{
 	int velocidad;// 1 a 10
 	int destreza; //1 a 5
 	int fuerza;//1 a 10
-	int Nivel; //1 a 10
-	int Armadura; //1 a 10
+	int aivel; //1 a 10
+	int armadura; //1 a 10
 }caract;
 
 typedef struct TPersonaje {
-	TDatos * DatosPersonales;
-	TCaracteristicas * Caracteristicas;
+	TDatos * datospersonales;
+	TCaracteristicas * caracteristicas;
 }personaje;
 
 
 int main()
 {
-	char Nombres[6][10]={“ale”, “b”, “c”, “d”, “e”};
-	char Apellidos[6][10]={“f”, “g”, “hormiga”, “i”, “j”};
+	char Nombres[6][10]={"Terry", "John", "Jacob", "Lorenzo", "Roberto"};
+	char Apellidos[6][10]={"Lopez","Messi", "Ronaldo", "Reus", "Tapp"};
 	return 0;
 }
