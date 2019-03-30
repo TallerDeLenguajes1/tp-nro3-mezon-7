cf146ef75547ebe0a139a0126ab349b26afab27a
Sam Ruby <agucho@hotmail.com>
2019-03-30 16:13:30 -0300

    Tarea terminada grupo 1

--- base.cpp
+++ base.cpp
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 enum TRaza{Orco, Humano, Mago, Enano, Elfo};
 void cargar_datos(struct TDatos * Character);
-//void mostrar_datos(struct TDatos * Character);
-
-//void cargar_caracteristicas(struct TCaracteristicas * Character);
-//void mostrar_caracteristicas(struct TCaracteristicas * Character);
+void mostrar_datos(struct TDatos * Character);
 
 
 typedef struct TDatos {
@@ -33,26 +31,37 @@ typedef struct TPersonaje
 }personaje;
 
 char Nombres[6][10]={"Terry", "John", "Jacob", "Lorenzo", "Roberto"};
-char Apellidos[6][10]={"Lopez","Messi", "Ronaldo", "Reus", "Tapp"};
+char Apellidos[6][10]={"Lopez ","Messi ", "Ronaldo ", "Reus ", "Tapp "};
 char Razas[6][10] = {"Orco", "Humano", "Mago", "Enano", "Elfo"};
 
 void main()
 {
 	srand(time(NULL));
-	
 	struct TDatos * Character= (struct TDatos *) malloc(sizeof(struct TDatos));
 	cargar_datos(Character);
+	mostrar_datos(Character);
 	
 }
 void cargar_datos(struct TDatos * Character)
 {
-	int random = rand() % 5;
-	
-	Character->raza = random;
-	strcpy(Nombres[random])
-	Character->ApellidoNombre = Nombres[random];
-	printf("%s\n", Razas[Character->raza]);
-	printf("%s\n", Character->ApellidoNombre);
-
+	int random = rand() % 5;// PERSONAJE ALEATORIO
+	int edadrand = rand() % 300;//EDAD DE 0 A 300
+	Character->edad = edadrand;//CARGO LA EDAD
+	Character->raza = random;//CARGO LA RAZA DEL PERSONAJE ALEATORIO
+	Character->salud = 100;// SETEO LA SALUD A 100
+	char nombre[32], apellido[32];
+ 	strcpy(nombre, Nombres[random]);//COPIO LA CADENA NOMBRE[RAZA] EN NOMBRE
+ 	strcpy(apellido, Apellidos[random]);//COPIO LA CADENA APELLIDO[RAZA] EN APELLIDO
+ 	char apenom[65];
+ 	strcat(apellido,nombre);// CONCATENO LA CADENA APELLIDO Y LA CADENA NOMBRE
+ 	strcpy(apenom, apellido);// ESTE PASO PODRIA SER INECESARIO YA QUE MI CADENA COPIADA ESTA EN LA CADENA APELLIDO
+	Character->ApellidoNombre = apenom;
 }
-//void mostrar_datos(struct TDatos * Character)
+
+void mostrar_datos(struct TDatos * Character)
+{
+	printf("Raza : %s\nApellido y Nombre: ", Razas[Character->raza]);//MUESTRO RAZA Y LA CADENA APELLIDONOMBRE
+	puts(Character->ApellidoNombre);
+	printf("Edad : %d \n", Character->edad);//MUESTRO EDAD
+	printf("Salud : %2.f \n", Character->salud);// MUESTRO SALUD
+}
