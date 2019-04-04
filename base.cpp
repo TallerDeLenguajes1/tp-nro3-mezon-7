64d2192aec7980b53da7252ea3e5f0a8abae9a0e
Sam Ruby <agucho@hotmail.com>
2019-04-04 20:06:18 -0300

    Grupo 1_3 comiteando

--- base.cpp
+++ base.cpp
@@ -5,64 +5,105 @@
 
 enum TRaza{Orco, Humano, Mago, Enano, Elfo};
 
-void cargar_datos(struct TDatos * datospersonales);
-void mostrar_datos(struct TDatos * datospersonales);
-
-
-typedef struct TDatos {
-	TRaza raza; //nota 1
+struct TDatos 
+{
+	TRaza raza ; //nota 1
+	//int raza;
 	char * ApellidoNombre; //nota 2
 	int edad; //entre 0 a 300
 	double salud;//100
-}tdatos;
+};
 
-typedef struct TCaracteristicas
+struct TCaracteristicas
 {
 	int velocidad;// 1 a 10
 	int destreza; //1 a 5
 	int fuerza;//1 a 10
 	int nivel; //1 a 10
 	int armadura; //1 a 10
-}caract;
+};
 
-typedef struct TPersonaje 
+struct TPersonaje
 {
 	TDatos * DatosPersonales;
 	TCaracteristicas * caracteristicas;
-}personaje;
-
-void cargar_caracteristicas(caract * cp);
-void mostrar_caracteristicas(caract * cp);
-
-
+};
 
 char Nombres[6][10]={"Terry", "John", "Jacob", "Lorenzo", "Roberto"};
 char Apellidos[6][10]={"Lopez ","Messi ", "Ronaldo ", "Reus ", "Tapp "};
 char Razas[6][10] = {"Orco", "Humano", "Mago", "Enano", "Elfo"};
+void cargar_datos(struct TDatos * Character);
+void mostrar_datos(struct TDatos * Character);
+void cargar_caracteristicas(struct TCaracteristicas  * cp);
+void mostrar_caracteristicas(struct TCaracteristicas * cp);
 
 void main()
 {
+	
 	srand(time(NULL));
-	struct TDatos * Character= (struct TDatos *) malloc(sizeof(struct TDatos));
-	caract * cp= (caract *) malloc(sizeof(caract));
-	cargar_caracteristicas(cp);
-	mostrar_caracteristicas(cp);
-	cargar_datos(Character);
-	mostrar_datos(Character);	
+	int cantidad;
+	printf("Ingrese N Personajes a Cargar\n");
+	scanf("%d", &cantidad); //5	
+	TPersonaje * jugadores = (TPersonaje *) malloc (sizeof(TPersonaje) * cantidad);
+	for(int i = 0; i < cantidad; i++)
+	{
+		
+		jugadores[i].caracteristicas = (TCaracteristicas *) malloc(sizeof(TCaracteristicas));
+		jugadores[i].DatosPersonales = (TDatos *) malloc(sizeof(TDatos));
+		cargar_caracteristicas(jugadores[i].caracteristicas);
+		cargar_datos(jugadores[i].DatosPersonales);
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		printf("Ingrese Personaje a Mostrar\n\n");
+		int num;
+		scanf("%d", &num);
+		mostrar_caracteristicas(jugadores[num].caracteristicas);
+		mostrar_datos(jugadores[num].DatosPersonales);
+	}
 }
+
 void cargar_datos(struct TDatos * Character)
 {
 	int random = rand() % 5;// PERSONAJE ALEATORIO
 	int edadrand = rand() % 300;//EDAD DE 0 A 300
+	switch (random)
+	{
+		case 0:{
+			Character->raza = Orco;
+			break;
+		}
+		case 1:{
+			Character->raza = Humano;
+			break;
+		}
+		case 2:{
+			Character->raza = Mago;
+			break;
+		}
+		case 3:{
+			Character->raza = Enano;
+			break;
+		}
+		case 4:{
+			Character->raza = Elfo;
+			break;
+		}
+	}
 	Character->edad = edadrand;//CARGO LA EDAD
-	Character->raza = random;//CARGO LA RAZA DEL PERSONAJE ALEATORIO
 	Character->salud = 100;// SETEO LA SALUD A 100
-	char nombre[32], apellido[32];
- 	strcpy(nombre, Nombres[random]);//COPIO LA CADENA NOMBRE[RAZA] EN NOMBRE
- 	strcpy(apellido, Apellidos[random]);//COPIO LA CADENA APELLIDO[RAZA] EN APELLIDO
+
+	/*char nombre[32], apellido[32];
+ 	strcpy(nombre, Nombres[Character->raza]);//COPIO LA CADENA NOMBRE[RAZA] EN NOMBRE
+ 	strcpy(apellido, Apellidos[Character->raza]);//COPIO LA CADENA APELLIDO[RAZA] EN APELLIDO
  	char apenom[65];
  	strcat(apellido,nombre);// CONCATENO LA CADENA APELLIDO Y LA CADENA NOMBRE
- 	strcpy(apenom, apellido);// ESTE PASO PODRIA SER INECESARIO YA QUE MI CADENA COPIADA ESTA EN LA CADENA APELLIDO
+ 	strcpy(apenom, apellido);// ESTE PASO PODRIA SER INECESARIO YA QUE MI CADENA COPIADA ESTA EN LA CADENA APELLIDO*/
+	char apenom[32];
+	int tipo = Character->raza;
+	strcpy(apenom, Apellidos[tipo]);
+	strcat(apenom , Nombres[tipo]);
+	puts(apenom);
 	Character->ApellidoNombre = apenom;
 }
 
@@ -74,7 +115,8 @@ void mostrar_datos(struct TDatos * Character)
 	printf("Salud : %2.f \n", Character->salud);// MUESTRO SALUD
 }
 
-void cargar_caracteristicas(caract * cp){
+void cargar_caracteristicas(struct TCaracteristicas * cp)
+{
 	printf("Cargando datos del personaje, por favor espere.\n");
 	cp->velocidad=1 + rand() % (11-1);
 	cp->destreza= 1 + rand() % (6-1);
@@ -83,7 +125,8 @@ void cargar_caracteristicas(caract * cp){
 	cp->armadura= 1 + rand() % (11-1);
 }
 
-void mostrar_caracteristicas(caract * cp){
+void mostrar_caracteristicas(struct TCaracteristicas * cp)
+{
 	printf("Velocidad: %d\n", cp->velocidad);
 	printf("Destreza: %d\n", cp->destreza);
 	printf("Fuerza: %d\n", cp->fuerza);
